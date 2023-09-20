804b3f8105811384dea30496508551269b03932f
sneha1413 <91750038+sneha1413@users.noreply.github.com>
2023-09-20 12:36:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const url = 'mongodb+srv://snehadabbiru:Snehavit14@cluster0.ntc74oz.mongodb.net/sneha?retryWrites=true&w=majority'
 const app = express()
 
-mongoose.connect(url,{useNewUrlParser:true})
+mongoose.connect(url,{ useNewUrlParser: true,
+  useUnifiedTopology: true,})
 const con = mongoose.connection
 con.on('error', console.error.bind(console, 'MongoDB connection error:'));
 con.once('open', () => {
