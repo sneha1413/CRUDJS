33d2e3f0c54243dd1d706abd79cc4a201d9631d3
sneha1413 <2003sneha2003@gmail.com>
2023-09-20 12:05:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb+srv://snehadabbiru:Snehavit14@cluster0.ntc74oz.mongodb.net/sneha?retryWrites=true&w=majority'
+const app = express()
+
+mongoose.connect(url,{useNewUrlParser:true})
+const con = mongoose.connection
+con.on('error', console.error.bind(console, 'MongoDB connection error:'));
+con.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.use(express.json())
+const aleinRouter = require('./routers/aliens')
+app.use('/aliens',aleinRouter)
+
+app.listen(9000,()=> {
+console.log('Server started')
+})
+
